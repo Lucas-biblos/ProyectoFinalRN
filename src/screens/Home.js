8a94b6f9efe1c223d22118343a28eb1721814c2c
Lucas-biblos <lbiblos@udesa.edu.ar>
2024-11-21 11:35:18 -0300

    terminado el home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,87 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { db, auth } from '../firebase/config';
+
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      loading: true,
+    };
+  }
+
+
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    db.collection('posts').orderBy('createdAt', 'desc').onSnapshot((docs) => {
+      let posts = [];
+      docs.forEach((doc) => {
+        posts.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      this.setState({
+        posts: posts,
+        loading: false,
+      });
+    });
+  }
+
+
   render() {
     return (
-        <Text>Home</Text>
-    )
+      <View style={styles.container}>
+        {!this.state.loading && (
+          <FlatList
+            data={this.state.posts}
+            keyExtractor={(post) => post.id}
+            renderItem={({ item }) => (
+              <View style={styles.postContainer}>
+                <Text style={styles.owner}>{item.data.owner}</Text>
+                <Text style={styles.description}>{item.data.description}</Text>
+              </View>
+            )}
+          />
+        )}
+      </View>
+    );
   }
 }
 
-export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#15202B',
+    padding: 10,
+  },
+  postContainer: {
+    backgroundColor: '#192734',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  owner: {
+    fontSize: 16,
+    color: '#1DA1F2',
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  description: {
+    fontSize: 14,
+    color: '#E1E8ED',
+    lineHeight: 20,
+  },
+});
+
+
+export default Home;
